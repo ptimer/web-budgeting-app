f3a1aae8d9922a9b1140e365d35e4b336887594e
ptimer <kriminalmykriminal@gmail.com>
2024-10-15 00:58:53 +0200

    feat: deleteItem util

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,3 +2,12 @@
 export const fetchData = (key: string): string => {
     return JSON.parse(JSON.stringify(localStorage.getItem(key) || ''));
 };
+
+// delete item
+interface DeleteItemProps {
+    key: string;
+}
+
+export const deleteItem = ({ key }: DeleteItemProps) => {
+    return localStorage.removeItem(key);
+}

26cb33c74734e2b531fe5fe9b154464eed16662c
ptimer <kriminalmykriminal@gmail.com>
2024-10-15 20:07:19 +0200

    feat: createBudget, generateRandomColor utils

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,3 +1,10 @@
+const generateRandomColor = () => {
+    const existingBudgetsLength = fetchData("budgets")?.length ?? 0;
+
+    return `${existingBudgetsLength * 34} 65% 50%`;
+}
+
+
 // Local storage
 export const fetchData = (key: string): string | null => {
     const item = localStorage.getItem(key);
@@ -6,6 +13,30 @@ export const fetchData = (key: string): string | null => {
 
     return JSON.parse(item);
 };
+
+// create budget
+interface Budget {
+    id: string;
+    name: string;
+    amount: number;
+    createdAt: Date;
+    color: string;
+}
+
+export const createBudget = ({ name, amount }: Pick<Budget, 'name' | 'amount'>) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name,
+        amount: +amount,
+        createdAt: Date.now(),
+        color: generateRandomColor(),
+    };
+
+    const existingBudgets = fetchData("budgets") ?? [];
+    const newBudgets = JSON.stringify([...existingBudgets, newItem]);
+
+    return localStorage.setItem("budgets", newBudgets);
+}
 
 // delete item
 interface DeleteItemProps {

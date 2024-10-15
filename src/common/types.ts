81ae083bd7772773dd2a2d85d04b6b1053b6ea93
ptimer <kriminalmykriminal@gmail.com>
2024-10-15 23:47:40 +0200

    feat: add Expense type

--- src/common/types.ts
+++ src/common/types.ts
@@ -7,19 +7,27 @@ export interface Budget {
     color: string;
 };
 
+export interface Expense {
+    id: string;
+    name: string;
+    amount: number;
+    createdAt: Date;
+    budgetId: string;
+};
+
 export enum APP_DATA_KEYS {
     userName = "userName",
-    budgets = "budgets"
+    budgets = "budgets",
+    expenses = "expenses",
 }
 
 export interface AppData {
     userName: string | null;
     budgets: Budget[];
+    expenses: Expense[];
 };
 
 // Site Config
 export interface SiteConfigType {
     companyName: string;
 }
-
-// 

b1c4de4a7c3af1a91a8ae115cd0d2458f5ee09fa
ptimer <kriminalmykriminal@gmail.com>
2024-10-15 22:57:20 +0200

    feat: clearing add budget form

--- src/components/AddBudgetForm.tsx
+++ src/components/AddBudgetForm.tsx
@@ -1,48 +1,80 @@
+import { useEffect, useRef } from "react";
+
 // rrd
-import { Form } from "react-router-dom";
+import { Form, useFetcher } from "react-router-dom";
 
 // library
 import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
 
 const AddBudgetForm = () => {
-  return (
-    <div className="form-wrapper">
-        <h2 className="h3">
-            Create budget
-        </h2>
-        <Form
-            method="post"
-            className="grid-sm"
-        >
-            <div className="grid-xs">
-                <label htmlFor="newBudget">Budget Name</label>
-                <input 
-                    type="text" 
-                    id="newBudget" 
-                    name="newBudget"
-                    placeholder="e.g., Groceries"
-                    required 
-                />
-            </div>
-            <div className="grid-xs">
-                <label htmlFor="newBudgetAmount">Amount</label>
-                <input 
-                    type="number"
-                    step="0.01" 
-                    id="newBudgetAmount" 
-                    name="newBudgetAmount"
-                    placeholder="e.g., $350"
-                    required
-                    inputMode="decimal"
-                />
-            </div>
-            <input type="hidden" name="_action" value="createBudget" />
-            <button type="submit" className="btn btn--dark">
-                <span>Create budget</span>
-                <CurrencyDollarIcon />
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state === "submitting";
+
+    const formRef = useRef<any>();
+    const focusRef = useRef<any>();
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset();
+            focusRef.current.focus();
+        }
+    }, [isSubmitting]);
+
+    const composeCreateBudgetButton = () => {
+        return (
+            <button 
+                type="submit" 
+                className="btn btn--dark" 
+                disabled={isSubmitting}
+            >
+                { isSubmitting ? <span>Submitting budget...</span>
+                : (
+                    <>
+                        <span>Create budget</span>
+                        <CurrencyDollarIcon />
+                    </>
+                  )}
             </button>
-        </Form>
-    </div>
+        )
+    }
+
+    return (
+        <div className="form-wrapper">
+            <h2 className="h3">
+                Create budget
+            </h2>
+            <fetcher.Form
+                method="post"
+                className="grid-sm"
+                ref={formRef}
+            >
+                <div className="grid-xs">
+                    <label htmlFor="newBudget">Budget Name</label>
+                    <input 
+                        type="text" 
+                        id="newBudget" 
+                        name="newBudget"
+                        placeholder="e.g., Groceries"
+                        required
+                        ref={focusRef}
+                    />
+                </div>
+                <div className="grid-xs">
+                    <label htmlFor="newBudgetAmount">Amount</label>
+                    <input 
+                        type="number"
+                        step="0.01" 
+                        id="newBudgetAmount" 
+                        name="newBudgetAmount"
+                        placeholder="e.g., $350"
+                        required
+                        inputMode="decimal"
+                    />
+                </div>
+                <input type="hidden" name="_action" value="createBudget" />
+                { composeCreateBudgetButton() }
+            </fetcher.Form>
+        </div>
   )
 }
 

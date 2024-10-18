19b13701c6457a36ff44e25512571b117075df11
ptimer <kriminalmykriminal@gmail.com>
2024-10-18 14:38:52 +0200

    feat: repositoryUrl -> siteConfig

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,4 +3,5 @@ import { SiteConfigType } from "@/common/types";
 
 export const SiteConfig: SiteConfigType = {
     companyName: "Budgefy",
+    repositoryUrl: "https://github.com/ptimer/web-budgeting-app",
 }

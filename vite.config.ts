163ebe3a3a74469c389306adc4638dcfec6e05f7
ptimer <kriminalmykriminal@gmail.com>
2024-10-18 15:29:18 +0200

    fix: name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "web-budgeting-app",
+  base: "/web-budgeting-app",
   resolve: {
     alias: {
       '@': '/src',
